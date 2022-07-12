eb917c366a085aa8f4a8aecb60344eab1f1a5759
afomichev <afomichev@ngpinform.ru>
2022-07-12 17:08:12 +0500

    add redis at hm3_task_1

--- Week_3/hm3_task_1.py
+++ Week_3/hm3_task_1.py
@@ -1,3 +1,5 @@
+import redis
+
 def cash_result(func):
     """
     функция-декоратор сохраняет (кэширует) результат декорируемой функции
@@ -17,6 +19,32 @@ def multiplier(number: int):
     return number * 2
 
 
+def cash_redis(func):
+    """
+    функция-декоратор сохраняет (кэширует) результат декорируемой функции
+    """
+
+    def wrapper(*args):
+        res = int(redis_client.get(name=str(*args)))
+        if not res:
+            res = func(*args)
+            redis_client.set(name=str(*args), value=res)
+        return res
+
+    return wrapper
+
+
+@cash_redis
+def multiplier2(number: int):
+    return number * 2
+
+
 if __name__ == "__main__":
-    [print(multiplier(i)) for i in range(5)]
-    [print(multiplier(i)) for i in range(5)]
+    # [print(multiplier(i)) for i in range(5)]
+    # [print(multiplier(i)) for i in range(5)]
+
+    redis_client = redis.Redis(host="localhost", port=6379, db=0)
+    [print(multiplier2(i)) for i in range(5)]
+    [print(multiplier2(i)) for i in range(5)]
+    redis_client.close()
+
